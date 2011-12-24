439d356635631fbf6355fdb0664f14af9c8a2b2f
Paul Armstrong <paul@paularmstrongdesigns.com>
2011-12-23 17:29:20 -0800

    tabs to spaces. ugh. stupid textmate 2

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 all: install setup
 
 install:
+	@chmod a+x scripts/*
 	@./scripts/install.sh
-	
+
 setup:
+	@chmod a+x scripts/*
 	@./scripts/setup.sh
-	
+
 .PHONY: all setup
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,8 @@
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "scripts/" --exclude "osx/" --exclude "Makefile" --exclude "README.md" -av . ~/.
-  	source "$HOME/.bash_profile"
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "scripts/" --exclude "osx/" --exclude "Makefile" --exclude "README.md" -av . ~/.
+      source "$HOME/.bash_profile"
 else
 echo "NOOOOOOOOOOOO!"
 fi
