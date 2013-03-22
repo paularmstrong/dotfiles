0375e8e904fef8930f74d27179e4516d74985abe
Paul Armstrong <paul@paularmstrongdesigns.com>
2013-03-22 12:59:09 -0700

    fix make completion

--- .bash/completion_scripts/make.sh
+++ .bash/completion_scripts/make.sh
@@ -1,1 +1,1 @@
-complete -W "`test -e Makefile && grep -oE '^[a-zA-Z0-9-]+:([^=]|$)' Makefile | sed 's/[^a-zA-Z0-9-]*$//'`" make
+complete -W "`test -e Makefile && grep -oE '^[a-zA-Z0-9_-]+:([^=]|$)' Makefile | sed 's/[^a-zA-Z0-9_-]*$//'`" make
