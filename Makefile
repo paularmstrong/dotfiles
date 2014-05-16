19effd48fc1290d8980e483160d2f3e80c86a4b5
Paul Armstrong <paul@paularmstrongdesigns.com>
2014-05-16 14:45:16 -0700

    make install script not require prompts

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
+NAME=Paul Armstrong
+EMAIL=paul@paularmstrongdesigns.com
+
 basic:
 	@chmod a+x scripts/*
-	@./scripts/install.sh
+	@./scripts/install.sh --name="${NAME}" --email="${EMAIL}"
 
 osx:
 	@chmod a+x osx/*
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    read -p "Do you want to update your git config? (y/n) " -n 1
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        TEMP="temp.gitconfig"
-        read -p "Enter your name: "
-        sed -e "s/<git.name>/$REPLY/g" .gitconfig > $TEMP && mv $TEMP .gitconfig
-        read -p "Enter your email address: "
-        sed -e "s/<git.email>/$REPLY/g" .gitconfig > $TEMP && mv $TEMP .gitconfig
-        read -p "Enter your github username: "
-        sed -e "s/<github.user>/$REPLY/g" .gitconfig > $TEMP && mv $TEMP .gitconfig
-        read -p "Enter your github token: "
-        sed -e "s/<github.token>/$REPLY/g" .gitconfig > $TEMP && mv $TEMP .gitconfig
-        echo
-    else
-        rm .gitconfig
-    fi
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "scripts/" --exclude "osx/" --exclude "Makefile" --exclude "README.md" -av . ~/.
-    source "$HOME/.bash_profile"
-    git checkout .gitconfig
-else
-echo "NOOOOOOOOOOOO!"
-fi
+TEMP="temp.gitconfig"
+
+while test $# -gt 0; do
+    case "$1" in
+        --name*)
+            NAME=`echo $1 | sed -e 's/^[^=]*=//g'`
+            sed -e "s/<git.name>/$NAME/g" .gitconfig > $TEMP && mv $TEMP .gitconfig
+            shift
+            ;;
+        --email*)
+            EMAIL=`echo $1 | sed -e 's/^[^=]*=//g'`
+            sed -e "s/<git.email>/$EMAIL/g" .gitconfig > $TEMP && mv $TEMP .gitconfig
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+rsync --exclude ".git/" --exclude ".DS_Store" --exclude "scripts/" --exclude "osx/" --exclude "Makefile" --exclude "README.md" -av . ~/.
+source "$HOME/.bash_profile"
+git checkout .gitconfig
